0674561451742fff7bd6f5f992fd4a8d82a7c611
sanket <smdetroja710@gmail.com>
2023-02-16 11:48:13 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,6 @@ import { Button, Code, Title, MantineProvider } from '@mantine/core';
 
 function Demo() {
   return (
-    <MantineProvider
-      theme={{
-        fontFamily: 'Verdana, sans-serif',
-        fontFamilyMonospace: 'Monaco, Courier, monospace',
-        headings: { fontFamily: 'Greycliff CF, sans-serif' },
-      }}
-    >
-      <Title order={3}>Greycliff CF or sans-serif title</Title>
-      <Button>Verdana button</Button>
-      <Code>Monaco, Courier Code</Code>
-    </MantineProvider>
+    <h1>Hello word</h1>
   );
 }
