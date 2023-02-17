694f95aed5e875a2a48d2159f69c3a9cce071e84
L <iussavani2@gmail.com>
2023-02-17 15:48:47 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,87 +1,93 @@
-import { createStyles, Overlay, Container, Title, Button, Text } from '@mantine/core';
-
-const useStyles = createStyles((theme) => ({
-  hero: {
-    position: 'relative',
-    backgroundImage:
-      'url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-
-  container: {
-    height: '100svh',
+import {
+  createStyles,
+  Image,
+  Container,
+  Title,
+  Button,
+  Group,
+  Text,
+  List,
+  ThemeIcon
+} from '@mantine/core'
+import { IconCheck } from '@tabler/icons-react'
+import image from '../assets/image.svg'
+const useStyles = createStyles(theme => ({
+  inner: {
     display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start',
-    paddingBottom: theme.spacing.xl * 6,
-    zIndex: 1,
-    position: 'relative',
-
-    [theme.fn.smallerThan('sm')]: {
-      height: 500,
-      paddingBottom: theme.spacing.xl * 3,
-    },
+    justifyContent: 'space-between',
+    paddingTop: theme.spacing.xl * 4,
+    paddingBottom: theme.spacing.xl * 4
   },
-
-  title: {
-    color: theme.white,
-    fontSize: 60,
-    fontWeight: 900,
-    lineHeight: 1.1,
-
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: 40,
-      lineHeight: 1.2,
-    },
-
-    [theme.fn.smallerThan('xs')]: {
-      fontSize: 28,
-      lineHeight: 1.3,
-    },
-  },
-
-  description: {
-    color: theme.white,
-    maxWidth: 600,
-
-    [theme.fn.smallerThan('sm')]: {
+  content: {
+    maxWidth: 540,
+    marginRight: theme.spacing.xl * 3,
+    [theme.fn.smallerThan('md')]: {
       maxWidth: '100%',
-      fontSize: theme.fontSizes.sm,
-    },
+      marginRight: 0
+    }
   },
-
+  title: {
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: 44,
+    lineHeight: 1.2,
+    fontWeight: 900,
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: 28
+    }
+  },
   control: {
-    marginTop: theme.spacing.xl * 1.5,
-
-    [theme.fn.smallerThan('sm')]: {
-      width: '100%',
-    },
+    [theme.fn.smallerThan('xs')]: {
+      flex: 1
+    }
   },
-}));
+  image: {
+    alignSelf: 'center',
+    flex: 1,
+    [theme.fn.smallerThan('md')]: {
+      display: 'none'
+    }
+  },
+  highlight: {
+    position: 'relative',
+    backgroundColor: theme.fn.variant({
+      variant: 'light',
+      color: theme.primaryColor
+    }).background,
+    borderRadius: theme.radius.sm,
+    padding: '4px 12px'
+  }
+}))
 
-export function Hero() {
-  const { classes } = useStyles();
-
+export function Hero () {
+  const { classes } = useStyles()
   return (
-    <div className={classes.hero}>
-      <Overlay
-        gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
-        opacity={1}
-        zIndex={0}
-      />
-      <Container className={classes.container}>
-        <Title className={classes.title}>A fully featured React components library</Title>
-        <Text className={classes.description} size="xl" mt="xl">
-          Build fully functional accessible web applications faster than ever – Mantine includes
-          more than 120 customizable components and hooks to cover you in any situation
-        </Text>
+    <div>
+      <Container size='lg'>
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              Join <span className={classes.highlight}>Coupon Trader</span>
+              <br /> Trade Your Unused Coupon Today
+            </Title>
+            <Text color='dimmed' mt='md'>
+              Looking to sell your unused coupons? Join Coupon Trader and take
+              advantage of our user-friendly platform, multiple payment options,
+              and dedicated customer support team. With our secure system, you
+              can easily turn your unused coupons into valuable resources. Don't
+              let those unwanted coupons go to waste - join our community today
+              and start trading with confidence!
+            </Text>
 
-        <Button variant="gradient" size="xl" radius="xl" className={classes.control}>
-          Get started
-        </Button>
+            <Group mt={30}>
+              <Button radius='xl' size='md' className={classes.control}>
+                Get started
+              </Button>
+            </Group>
+          </div>
+          <Image src={image} className={classes.image} />
+        </div>
       </Container>
     </div>
-  );
+  )
 }
