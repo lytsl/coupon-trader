27eeb4afb62ef366e8a4a57ff40820138a76e80d
L <iussavani2@gmail.com>
2023-02-23 19:29:57 +0530

    react query

--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -1,15 +1,14 @@
-import { Loader } from '@mantine/core'
-
 import {
   UserResponse,
   LoginDTO,
   RegisterDTO,
-  AuthUser,
   loginWithEmailAndPassword,
   getUser,
   registerWithEmailAndPassword,
 } from 'features/auth/api'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import storage from 'lib/storage'
+import { useCallback } from 'react'
 
 async function handleUserResponse(data: UserResponse) {
   const { jwt, user } = data
@@ -17,48 +16,80 @@ async function handleUserResponse(data: UserResponse) {
   return user
 }
 
-async function loadUser() {
-  if (storage.getToken()) {
-    const data = await getUser()
-    return data
+const queryClient = useQueryClient()
+
+const setUser = useCallback(
+  (data: any) => queryClient.setQueryData(userKey, data),
+  [queryClient],
+)
+
+const userKey = ['authenticated-user']
+
+const useUser = () =>
+  useQuery(['authenticated-user'], async () => {
+    if (storage.getToken()) {
+      const data = await getUser()
+      return data
+    }
+    return null
+  })
+
+const useLogin = () => {
+  return useMutation(
+    async (data: LoginDTO) => {
+      const response = await loginWithEmailAndPassword(data)
+      const user = await handleUserResponse(response)
+      return user
+    },
+    {
+      onSuccess: (user) => setUser(user),
+    },
+  )
+}
+
+const useRegister = () => {
+  return useMutation({
+    mutationFn: async (data: RegisterDTO) => {
+      const response = await registerWithEmailAndPassword(data)
+      const user = await handleUserResponse(response)
+      return user
+    },
+    onSuccess: (user) => setUser(user),
+  })
+}
+
+const useLogout = () => {
+  return useMutation({
+    mutationFn: async () => {
+      storage.clearToken()
+      window.location.assign(window.location.origin as unknown as string)
+    },
+    onSuccess: (user) => setUser(user),
+  })
+}
+
+const AuthLoader = ({
+  children,
+  renderLoading,
+  renderUnauthenticated,
+}: {
+  children: React.ReactNode
+  renderLoading: () => JSX.Element
+  renderUnauthenticated?: () => JSX.Element
+}) => {
+  const { isSuccess, isFetched, status, data } = useUser()
+  if (isSuccess) {
+    if (renderUnauthenticated && !data) {
+      return renderUnauthenticated()
+    }
+    return <>{children}</>
   }
-  return null
+
+  if (!isFetched) {
+    return renderLoading()
+  }
+
+  return <div>Unhandled status: {status}</div>
 }
 
-async function loginFn(data: LoginDTO) {
-  const response = await loginWithEmailAndPassword(data)
-  const user = await handleUserResponse(response)
-  return user
-}
-
-async function registerFn(data: RegisterDTO) {
-  const response = await registerWithEmailAndPassword(data)
-  const user = await handleUserResponse(response)
-  return user
-}
-
-async function logoutFn() {
-  storage.clearToken()
-  window.location.assign(window.location.origin as unknown as string)
-}
-
-const authConfig = {
-  loadUser,
-  loginFn,
-  registerFn,
-  logoutFn,
-  LoaderComponent() {
-    return (
-      <div className="w-screen h-screen flex justify-center items-center">
-        <Loader size="xl" />
-      </div>
-    )
-  },
-}
-
-// export const { AuthProvider, useAuth } = initReactQueryAuth<
-//   AuthUser | null,
-//   unknown,
-//   LoginCredentialsDTO,
-//   RegisterCredentialsDTO
-// >(authConfig);
+export { useUser, useLogin, useRegister, useLogout, AuthLoader }
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'jotai'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
